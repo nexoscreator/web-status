b9f8ae1391abc182ca23296c1b9d8f71f1ea6b2a
nexoscreator <nexoscreator@gmail.com>
2024-06-05 10:01:29 +0530

    update

---
+++ components/Footer.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="flex flex-col md:flex-row items-center justify-between my-20 space-y-2 md:space-x-2 md:space-y-0">
+    <p class="flex items-center w-max">
+      Coded with 💜<a class="ml-4" target="_blank" href="https://twitter.com/zernonia">by Nexos Creator</a>
+    </p>
+
+    <div class="flex items-center">
+      <a target="_blank" href="https://github.com/nexoscreator/" class="mr-4">GitHub</a>
+      <a href="https://www.nexoscreation.com/" target="_blank">Official Website</a>
+    </div>
+  </div>
+</template>
