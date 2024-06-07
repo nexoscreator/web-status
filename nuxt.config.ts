4110a361b8219a46011474520162ee72e5b00e95
Nexos Creators <112468782+nexoscreator@users.noreply.github.com>
2024-06-07 21:49:17 +0530

    Update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,6 @@
 import { defineNuxtConfig } from 'nuxt'
 
 export default defineNuxtConfig({
-  preset: 'github_pages',
   css: ["~/assets/main.css"],
   modules: ["nuxt-windicss", "@nuxt/content", "unplugin-icons/nuxt"]
 })
